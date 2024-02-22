b5ac3c54fe792fa50d99d62db139f53fda0e3d35
KTJ <jusin7@empal.com>
2024-02-22 17:08:22 +0900

    ALLOWD_HOSTS 변경

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t6*=z)sj*jn#pe*84gi4rpibacg_lehn)q0k=9jau09v60v)up
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['43.202.27.5']
 
 
 # Application definition
