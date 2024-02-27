f4b2837a09b951716eb46b60f61b0e02cb6201cd
KTJ <jusin7@empal.com>
2024-02-27 17:00:20 +0900

    로그 설정 수정

--- config/settings/base.py
+++ config/settings/base.py
@@ -177,7 +177,7 @@ LOGGING = {
             'filters': ['require_debug_false'],
             'class' : 'logging.handlers.RotatingFileHandler',
             'filename': BASE_DIR / 'logs/mysite.log',
-            'maxBytes': 1024*1024*5, # 5 MB
+            'maxBytes': 1024*1024*5,  # 5 MB
             'backupCount': 5,
             'formatter': 'standard',
         },
